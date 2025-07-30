4360edd419ddf45e9eaf7c6c6a7f467c4c0735ff
TheRealJ-OnCode <therealjonweb@gmail.com>
2025-07-30 13:21:07 +0300

    connect : db config added, server.js updated

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
+const connectDB = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.use(cors());
-app.use("/auth",authRoutes)
-app.get("/", (req, res) => {
-    res.send("🚧 Gatekit API is currently under development. Stay tuned for upcoming features!");
-})
-app.listen(PORT, () => { console.log("Gatekit Demo Running on : ", PORT) });
+(async () => {
+    await connectDB()
+    app.use("/auth", authRoutes)
+    app.get("/", (req, res) => {
+        res.send("🚧 Gatekit API is currently under development. Stay tuned for upcoming features!");
+    })
+    app.listen(PORT, () => { console.log("Gatekit Demo Running on : ", PORT) });
+})()
+
 
 
